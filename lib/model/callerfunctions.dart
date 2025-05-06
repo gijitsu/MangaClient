95552020b41cd70ab70c18efbc0ac8f239a95cac
gijitsu <bertmpngn@gmail.com>
2025-05-06 08:12:46 +0800

    changes on URL endpoint again

--- lib/model/callerfunctions.dart
+++ lib/model/callerfunctions.dart
@@ -2,97 +2,92 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
+
+const String baseWebUrl = 'proxy.bertmpngn.workers.dev';
+const String graphQLEndpointWeb = 'https://servermangaanime-zjgm.onrender.com/graphql';
+const String graphQLEndpointMobile = 'http://10.0.2.2:5000/graphql';
+
 Future<Map<String, dynamic>> graphqlQuery(
     String query, Map<String, dynamic> variables) async {
-  String theUrl = 'https://servermangaanime-zjgm.onrender.com/graphql';
-  if (!kIsWeb) {
-    theUrl = 'http://10.0.2.2:5000/graphql';
-  }
+  final url = kIsWeb ? graphQLEndpointWeb : graphQLEndpointMobile;
+
   final response = await http.post(
-    Uri.parse(theUrl),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: json.encode({
-      'query': query,
-      'variables': variables,
-    }),
+    Uri.parse(url),
+    headers: {'Content-Type': 'application/json'},
+    body: json.encode({'query': query, 'variables': variables}),
   );
 
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
-    throw Exception('Failed to load data');
+    throw Exception('GraphQL query failed with status: ${response.statusCode}');
   }
 }
 
-const baseUrl = 'proxy.bertmpngn.workers.dev';
-Future<http.Response> fetchTitle(title) {
+Future<http.Response> fetchTitle(String title) {
   final queryParameters = {"title": title};
-  final uri = Uri.https(baseUrl, '/manga', queryParameters);
+  final uri = Uri.https(baseWebUrl, '/manga', queryParameters);
   return http.get(uri);
 }
 
 Future<Map<String, dynamic>> fetchStatistics(String mangaId) async {
-  var url = Uri.parse('https://proxy.bertmpngn.workers.dev/statistics/manga/' + mangaId);
-  var response = await http.get(url);
+  final uri = Uri.https(baseWebUrl, '/statistics/manga/$mangaId');
+  final response = await http.get(uri);
 
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
-    throw Exception('Failed to load data');
+    throw Exception('Failed to fetch statistics');
   }
 }
 
-Future<http.Response> fetchGreatest(order, {String title = ' '}) {
-  Map<String, dynamic> queryParameters = {};
-  order.forEach((key, value) {
-    queryParameters["order[" + key + "]"] = value;
-  });
-  queryParameters['title'] = title;
-
-  final uri = Uri.https(baseUrl, '/manga', queryParameters);
-  return http.get(uri);
-}
-
-Future<http.Response> fetchTitleAdvanced(includedTags, excludedTags) {
-  final queryParameters = {
-    "includedTags[]": includedTags,
-    "excludedTags[]": excludedTags,
+Future<http.Response> fetchGreatest(Map<String, dynamic> order, {String title = ' '}) {
+  final queryParameters = <String, String>{
+    for (var key in order.keys) 'order[$key]': order[key].toString(),
+    'title': title,
   };
-  final uri = Uri.https(baseUrl, '/manga', queryParameters);
+  final uri = Uri.https(baseWebUrl, '/manga', queryParameters);
   return http.get(uri);
 }
 
-Future<http.Response> fetchCoverArt(coverId) {
-  final uri = Uri.https(baseUrl, '/cover/' + coverId);
+Future<http.Response> fetchTitleAdvanced(List<String> includedTags, List<String> excludedTags) {
+  final queryParameters = {
+    for (var tag in includedTags) 'includedTags[]': tag,
+    for (var tag in excludedTags) 'excludedTags[]': tag,
+  };
+  final uri = Uri.https(baseWebUrl, '/manga', queryParameters);
+  return http.get(uri);
+}
+
+Future<http.Response> fetchCoverArt(String coverId) {
+  final uri = Uri.https(baseWebUrl, '/cover/$coverId');
   return http.get(uri);
 }
 
 Future<http.Response> fetchTags() {
-  final uri = Uri.https(baseUrl, '/manga/tag');
+  final uri = Uri.https(baseWebUrl, '/manga/tag');
   return http.get(uri);
 }
 
-Future<http.Response> fetchChapters(id, {int offset = 0, int limit = 100}) {
-  Map<String, dynamic> queryParameters = {
+Future<http.Response> fetchChapters(String id, {int offset = 0, int limit = 100}) {
+  final queryParameters = {
     'order[volume]': 'asc',
     'order[chapter]': 'asc',
-    "translatedLanguage[]": "en"
+    'translatedLanguage[]': 'en',
+    'offset': offset.toString(),
+    'limit': limit.toString(),
   };
-  queryParameters['offset'] = offset.toString();
-  queryParameters['limit'] = limit.toString();
-  final uri = Uri.https(baseUrl, '/manga/' + id + '/feed', queryParameters);
+  final uri = Uri.https(baseWebUrl, '/manga/$id/feed', queryParameters);
   return http.get(uri);
 }
 
 Future<Map<String, dynamic>> makeGetRequest(String chapterId) async {
-  var url = Uri.parse('https://proxy.bertmpngn.workers.dev/at-home/server/' + chapterId);
-  var response = await http.get(url);
+  final uri = Uri.https(baseWebUrl, '/at-home/server/$chapterId');
+  final response = await http.get(uri);
 
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
-    throw Exception('Failed to load data');
+    throw Exception('Failed to load chapter data');
   }
 }
